255c2290a9b101bce4f4cc11e76394420c913c13
bruno-brazil <bruno.brazil2022@gmail.com>
2022-06-25 10:07:30 -0300

    Custom content for current user

--- backend/src/main/java/com/project/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/project/dslearn/repositories/NotificationRepository.java
@@ -1,11 +1,16 @@
 package com.project.dslearn.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project.dslearn.entities.Notification;
+import com.project.dslearn.entities.User;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	
+	Page<Notification> findByUser(User user, Pageable pageable);
 
 }
